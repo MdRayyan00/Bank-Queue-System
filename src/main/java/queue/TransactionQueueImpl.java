c06050a6df046e824f77f593edb893cdb587c82b
pranjal161 <161pranjal@gmail.com>
2023-02-22 12:03:47 +0000

    Adding comments

--- src/main/java/queue/TransactionQueueImpl.java
+++ src/main/java/queue/TransactionQueueImpl.java
@@ -8,26 +8,31 @@ import java.util.List;
 public class TransactionQueueImpl implements TransactionQueue {
     LinkedList<Transaction> queue = new LinkedList<>();
 
+    // Adding transaction in the Queue from the transactions file
     @Override
     public void push(Transaction transaction) {
         queue.push(transaction);
     }
 
+    // Each transaction is checked and then status of transaction is updated (Settled, Declined) according to the checks
     @Override
     public void processTransactions() {
 
     }
 
+    // The transactions which are marked as Declined are retrieved and are processed again
     @Override
     public List<Transaction> getTransactionsByStatus(String status) {
         return null;
     }
 
+    // Once all the transactions are processed the 'Declined' transactions are removed from the Queue.
     @Override
     public List<Transaction> removeTransactionsByStatus(String status) {
         return null;
     }
 
+    // All the 'Settled' transactions are displayed.
     @Override
     public List<Transaction> displayAll() {
         return null;
